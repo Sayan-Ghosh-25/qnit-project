5898f3b70650c32802582683c709856a00aec754
Sayan Ghosh <bandanagho674@gmail.com>
2025-09-27 12:10:08 +0530

    Update

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -50,7 +50,6 @@ export async function sendEmail(to, subject, html = null, opts = {}) {
         }),
     ...(opts.cc ? { cc: buildRecipientArray(opts.cc) } : {}),
     ...(opts.bcc ? { bcc: buildRecipientArray(opts.bcc) } : {}),
-    ...(opts.replyTo ? { replyTo: { email: opts.replyTo.email, name: opts.replyTo.name } } : {}),
   };
 
   if (opts.attachments && Array.isArray(opts.attachments) && opts.attachments.length) {
